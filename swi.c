72fdcef88db1bf045e7a7adb84bddd85dead2945
preetpal singh <preetpal.singh@.s.amity.edu>
2025-03-26 13:47:17 +0530

    case 1.3

--- swi.c
+++ swi.c
@@ -2,7 +2,11 @@ switch (choice) {
         case 1:
             printf("You selected B.Tech CSE.\n");
 	case 2:
-	   printf("selected for Btech AIML") break;
+	   printf("selected for Btech AIML");
+	case 3:
+	  printf("selected for btech ece");
+	  break;
 	    }
+
 return 0;
 }
